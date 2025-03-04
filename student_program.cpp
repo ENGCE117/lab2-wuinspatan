9ab1568bab6ff638bfd48ac680cf610388a5911a
VIN GOOD <153739263+wuinspatan@users.noreply.github.com>
2025-03-04 10:43:18 +0700

    Update student_program.cpp
    Rev 1.0
    4/3/2025
    10:43 AM

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[] ) ;
 
 int main() {
-    //YOUR CODE HERE
+    char text[50] ;  
+    char *output ;
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
+    
     return 0 ;
+}//end main function
+
+char* reverse (char str1[] ) {
+    printf("Enter str1 : ") ;
+    fgets(str1, 50, stdin) ;  
+
+    //delete newline
+    size_t len = strlen(str1) ; 
+    if (len > 0 && str1[len - 1] == '\n') {
+        str1[len - 1] = '\0' ;
+        len-- ;  
+    }
+    
+    //reverse text
+    for (int i = 0 ; i < len / 2 ; i++) {
+        char temp = str1[i] ;
+        str1[i] = str1[len - 1 - i] ;
+        str1[len - 1 - i] = temp ;
+    }
+
+    printf("FUNC : %s\n", str1) ;
+    return str1 ;
 }//end function
